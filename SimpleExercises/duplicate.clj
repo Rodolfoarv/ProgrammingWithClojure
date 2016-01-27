04826618b550b8f5ee23253d8c0b498eb4e9f8f8
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2016-01-26 18:18:16 -0600

    Added loop_recur to duplicate.clj and recursive.clj

--- SimpleExercises/duplicate.clj
+++ SimpleExercises/duplicate.clj
@@ -1,11 +1,22 @@
 ; ******* Rodolfo Andrés Ramírez Valenzuela
 
 
-(defn dup
+(defn dup_recursive
   "Returns a list with all the elements of x duplicated"
   [x]
   (if(empty? x)
     ()
     (cons (first x)
       (cons (first x)
-       (dup (rest x))))))
+       (dup_recursive (rest x))))))
+
+
+(defn dup_loop_recur
+  [x]
+  (loop [lst x
+         result ()]
+    (if (empty? lst)
+      (reverse result)
+      (recur (rest lst)
+                (cons (first lst)
+                  (cons (first lst) result))))))
--- SimpleExercises/recursive.clj
+++ SimpleExercises/recursive.clj
@@ -1,10 +1,15 @@
-(defn pow
+(defn pow_recursion
   "Raises b to the power of e"
   [b e]
   (if (zero? e)
     1N ;gives a Big Integer
     (* b (pow b(dec e)))))
 
-; lein repl
-; (load-file recursive.clj)
-;
+(defn pow_loop_recur
+  "Raises b to the power of e"
+  [b e]
+  (loop [ i e
+          r 1N]
+    (if (zero? i)
+      r
+      (recur (dec i) (* b r)))))
