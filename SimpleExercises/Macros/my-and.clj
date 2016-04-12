72b3fb51e8aa74d6e0dbf203d71a4912b0df7b14
Rodolfo <rodolfo@ramirezvalenzuela.com>
2016-04-12 17:42:26 -0500

    Implementation of my-and.clj

--- SimpleExercises/Macros/my-and.clj
+++ SimpleExercises/Macros/my-and.clj
@@ -1,3 +1,13 @@
 (defmacro my-and
-  [x y]
-  (list 'if x y false ))
+  "Evaluates all its arguments one at a time, from left to
+  right. If a form returns logical false (nil or false),
+  my-and returns that value and doesn't evaluate any of the
+  other expressions, otherwise it returns the value of the
+  last expr. (my-and) returns true."
+  ([] true)
+  ([x] x)
+  ([x & y]
+   `(let [temp# ~x]
+      (if temp#
+          (my-and ~@y)
+          temp#))))
