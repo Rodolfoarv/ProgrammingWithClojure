2d5708fb308bbf587cd1d429811ddc410b665c6f
Rodolfo <rodolfo@ramirezvalenzuela.com>
2016-04-27 11:46:45 -0500

    Initial implementation of batch

--- Assignments/DSL/scripting-dsl.clj
+++ Assignments/DSL/scripting-dsl.clj
@@ -25,3 +25,33 @@
   java.lang.Double
   [form]
   (str form))
+
+
+; Windows batch implementation
+
+(defmulti emit-batch
+  (fn [form] (class form)))
+
+(defmethod emit-batch
+  clojure.lang.PersistentList
+  [form]
+  (let [n (name (first form))]
+    (cond
+      (= n "println") (str "ECHO " (second form))
+      :else
+      nil)))
+
+(defmethod emit-batch
+  java.lang.String
+  [form]
+  form)
+
+(defmethod emit-batch
+  java.lang.Integer
+  [form]
+  (str form))
+
+(defmethod emit-batch
+  java.lang.Double
+  [form]
+  (str form))
