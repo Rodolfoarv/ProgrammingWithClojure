87e040f6a0bcc177888b62bc27b395c1f3b63af7
Rodolfo <rodolfo@ramirezvalenzuela.com>
2016-04-27 11:42:03 -0500

    Refactor with multimethod for bash scripts

--- Assignments/DSL/scripting-dsl.clj
+++ Assignments/DSL/scripting-dsl.clj
@@ -1,14 +1,27 @@
-(defn emit-bash-form [a]
-  (let [cl (class a)]
+(defmulti emit-bash
+  (fn [form]
+    (class form)))
+
+(defmethod emit-bash
+  clojure.lang.PersistentList
+  [form]
+  (let [n (name (first form))]
     (cond
-      (= cl clojure.lang.PersistentList)
-        (let [n (name (first a))]
-          (cond
-            (= n "println") (str "echo " (second a))
-            :else
-            nil))
-      (= cl java.lang.String) a
-      (= cl java.lang.Long) (str a)
-      (= cl java.lang.Double) (str a)
+      (= n "println") (str "echo " (second form))
       :else
       nil)))
+
+(defmethod emit-bash
+  java.lang.String
+  [form]
+  form)
+
+(defmethod emit-bash
+  java.lang.Integer
+  [form]
+  (str form))
+
+(defmethod emit-bash
+  java.lang.Double
+  [form]
+  (str form))
