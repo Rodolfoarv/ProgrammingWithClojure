b3517538c88d5b0bc1a36d7d0283bb90b579478f
Rodolfo <rodolfo@ramirezvalenzuela.com>
2016-04-27 11:35:56 -0500

    Added echo functionality for bash

--- Assignments/DSL/scripting-dsl.clj
+++ Assignments/DSL/scripting-dsl.clj
@@ -1,6 +1,12 @@
 (defn emit-bash-form [a]
   (let [cl (class a)]
     (cond
+      (= cl clojure.lang.PersistentList)
+        (let [n (name (first a))]
+          (cond
+            (= n "println") (str "echo " (second a))
+            :else
+            nil))
       (= cl java.lang.String) a
       (= cl java.lang.Long) (str a)
       (= cl java.lang.Double) (str a)
