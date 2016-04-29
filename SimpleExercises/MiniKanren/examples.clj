3de9e725f7cd81c3196888d59f2549fd19420fb5
Rodolfo <rodolfo@ramirezvalenzuela.com>
2016-04-29 18:39:38 -0500

    Added lasto with pattern matching

--- SimpleExercises/MiniKanren/examples.clj
+++ SimpleExercises/MiniKanren/examples.clj
@@ -6,13 +6,22 @@
     (empty? (rest lst)) (first lst)
     (not (empty? (rest lst))) (my-last (rest lst))))
 
-(defn lasto
+; (defn lasto
+;   [lst result]
+;   (fresh [h t]
+;     (conde
+;       [(conso result t lst) (== t [])] ;unification with empty list to ask if t is empty
+;       ;first option [(conso h t lst) (== t []) (== h result)  ]
+;       [(conso h t lst) (!= t []) (lasto t result)])))
+
+;Pattern matching
+
+(defne lasto
   [lst result]
-  (fresh [h t]
-    (conde
-      [(conso result t lst) (== t [])] ;unification with empty list to ask if t is empty
-      ;first option [(conso h t lst) (== t []) (== h result)  ]
-      [(conso h t lst) (!= t []) (lasto t result)])))
+  ([[h] h])
+  ([[h . t] result]
+    (!= t [])
+    (lasto t result)))
 
 (defn reverseo
   [lst result]
