16eda997f206799654cf6ed11c64a24a5431f5e6
Rodolfo <rodolfo@ramirezvalenzuela.com>
2016-04-29 18:00:22 -0500

    Added initial implementation minikanren examples.clj

--- SimpleExercises/MiniKanren/examples.clj
+++ SimpleExercises/MiniKanren/examples.clj
@@ -13,3 +13,21 @@
       [(conso result t lst) (== t [])] ;unification with empty list to ask if t is empty
       ;first option [(conso h t lst) (== t []) (== h result)  ]
       [(conso h t lst) (!= t []) (lasto t result)])))
+
+(defn reverseo
+  [lst result]
+  (fresh [h t x ]
+    (conde
+      [(== lst []) (== result [])]
+      [(!= lst [])
+      (conso h t lst)
+      (reverseo t x)
+      (appendo x [h] result)])))
+
+; First implementation of dup
+
+(defn dup
+  [lst]
+  (cond
+    (empty? lst) ()
+    (not (empty? lst)) (concat [(first lst) (first lst)] (dup (rest lst)))))
